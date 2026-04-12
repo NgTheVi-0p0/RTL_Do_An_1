519103e8360b0b1be37564a996d89b070c9692ae
Nguyên Thế Vĩ <23521775@gm.uit.edu.vn>
2026-04-13 00:55:23 +0700

    ngày 13/4 hoàn thành code khối imm_extend

--- src/imm_extend.v
+++ src/imm_extend.v
@@ -8,20 +8,15 @@ module imm_extend (
   begin
     case (imm_sel)
       // I-type
-      3'b000:
-        imm_ext = {{20{instr[31]}}, instr[31:20]};
+      3'b000: imm_ext = {{20{instr[31]}}, instr[31:20]};
       // S-type
-      3'b001:
-        imm_ext = {{20{instr[31]}}, instr[31:25], instr[11:7]};
+      3'b001: imm_ext = {{20{instr[31]}}, instr[31:25], instr[11:7]};
       // B-type:
-      3'b010:
-        imm_ext = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
+      3'b010: imm_ext = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
       // U-type:
-      3'b011:
-        imm_ext = {instr[31:12], 12'b0};
+      3'b011: imm_ext = {instr[31:12], 12'b0};
       // J-type:
-      3'b100:
-        imm_ext = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
+      3'b100: imm_ext = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
       default:
         imm_ext = 32'b0;
     endcase
